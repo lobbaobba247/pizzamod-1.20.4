195b3d88a37b56a2d9c0053bb923c8a230852909
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 19:29:24 +0000

    Initial Commit

--- src/main/java/blobbatobba/pizzamod/item/ModItems.java
+++ src/main/java/blobbatobba/pizzamod/item/ModItems.java
@@ -2,7 +2,10 @@ package blobbatobba.pizzamod.item;
 
 import blobbatobba.pizzamod.PizzaMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -10,6 +13,9 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings()));
 
+    private static void addItemsToFood_and_DrinksItemGroup(FabricItemGroupEntries entries) {
+        entries.add(TOMATO);
+    }
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(PizzaMod.MOD_ID, name), item);
@@ -17,5 +23,7 @@ public class ModItems {
 
     public static void registerModItems() {
         PizzaMod.LOGGER.info("Registering Mod Items for "  + PizzaMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToFood_and_DrinksItemGroup);
     }
 }
