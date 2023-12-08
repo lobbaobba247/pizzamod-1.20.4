26631c0c2150d368b733a738baeab3003d580751
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 19:25:05 +0000

    Initial Commit

--- src/main/java/blobbatobba/pizzamod/PizzaMod.java
+++ src/main/java/blobbatobba/pizzamod/PizzaMod.java
@@ -1,5 +1,6 @@
 package blobbatobba.pizzamod;
 
+import blobbatobba.pizzamod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ public class PizzaMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/blobbatobba/pizzamod/item/ModItems.java
+++ src/main/java/blobbatobba/pizzamod/item/ModItems.java
@@ -1,6 +1,21 @@
 package blobbatobba.pizzamod.item;
 
-public class ModItems {
+import blobbatobba.pizzamod.PizzaMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
-    public static void registerModItems
+public class ModItems {
+    public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(PizzaMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        PizzaMod.LOGGER.info("Registering Mod Items for "  + PizzaMod.MOD_ID);
+    }
 }
