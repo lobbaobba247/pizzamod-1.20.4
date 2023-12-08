a438aaf438ea6138bf65abafa13943899146057d
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 19:45:53 +0000

    Initial Commit

--- src/main/java/blobbatobba/pizzamod/item/ModFoodConponents.java
+++
@@ -1,7 +0,0 @@
-package blobbatobba.pizzamod.item;
-
-import net.minecraft.item.FoodComponent;
-
-public class ModFoodConponents {
-    public static FoodComponent TOMATO = new FoodComponent.Builder().hunger(3).saturationModifier(0.25f).build();
-}
