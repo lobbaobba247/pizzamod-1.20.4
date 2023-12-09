6571e365514e390ab597d36eaadc11d89a3a8980
BuildTools <unconfigured@null.spigotmc.org>
2023-12-09 14:31:13 +0000

    Initial Commit

--- src/main/java/blobbatobba/pizzamod/item/ModFoodComponents.java
+++ src/main/java/blobbatobba/pizzamod/item/ModFoodComponents.java
@@ -4,6 +4,8 @@ import blobbatobba.pizzamod.PizzaMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -15,11 +17,23 @@ public class ModFoodComponents {
         entries.add(TOMATO);
         entries.add(CHEESE);
         entries.add(PIZZA);
+        entries.add(Uncooked_PIZZA);
     }
 
     public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(3).saturationModifier(0.25f).build())));
+
     public static final Item CHEESE = registerItem("cheese", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).build())));
+
     public static final Item PIZZA = registerItem("pizza", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(20).saturationModifier(1f).build())));
+
+    public static final Item Uncooked_PIZZA = registerItem("uncooked_pizza", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(0)
+            .saturationModifier(1f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 1200),1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 600), 1f).build())));
+
+
+
+
+
     public static void registerModItems() {
         PizzaMod.LOGGER.info("Registering Mod Items for "  + PizzaMod.MOD_ID);
 
