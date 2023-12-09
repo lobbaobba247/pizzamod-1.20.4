3b48dce44eafb51b1ddd8224c0af4af7eb5d8ce9
BuildTools <unconfigured@null.spigotmc.org>
2023-12-09 12:44:06 +0000

    Initial Commit

--- src/main/java/blobbatobba/pizzamod/item/ModFoodComponents.java
+++ src/main/java/blobbatobba/pizzamod/item/ModFoodComponents.java
@@ -7,17 +7,17 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-
 import static blobbatobba.pizzamod.item.ModItems.registerItem;
 
 public class ModFoodComponents {
 
     private static void addItemsToFood_and_DrinksItemGroup(FabricItemGroupEntries entries) {
         entries.add(TOMATO);
+        entries.add(CHEESE);
     }
 
     public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(3).saturationModifier(0.25f).build())));
-
+    public static final Item CHEESE = registerItem("cheese", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(2).saturationModifier(0.3f).build())));
     public static void registerModItems() {
         PizzaMod.LOGGER.info("Registering Mod Items for "  + PizzaMod.MOD_ID);
 
