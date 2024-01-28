0a5d8df043a1d00dc998240c8e02ad15bb34744c
BuildTools <unconfigured@null.spigotmc.org>
2024-01-28 17:26:24 +0000

    Block of Pizza

--- src/main/java/blobbatobba/pizzamod/block/ModBlocks.java
+++ src/main/java/blobbatobba/pizzamod/block/ModBlocks.java
@@ -16,7 +16,7 @@ public class ModBlocks {
 
 
     public static final Block TOMATO_CROP = Registry.register(Registries.BLOCK, new Identifier(PizzaMod.MOD_ID, "tomato_crop"), new TomatoCropBlock(FabricBlockSettings.copyOf(Blocks.WHEAT)));
-    public static final Block BLOCK_OF_PIZZA = new Block(FabricBlockSettings.create().notSolid().strength(1.0f).mapColor(MapColor.BLACK).sounds(BlockSoundGroup.GRAVEL));
+    public static final Block BLOCK_OF_PIZZA = new Block(FabricBlockSettings.create().notSolid().strength(1.0f, 1.0f).mapColor(MapColor.TERRACOTTA_BROWN).sounds(BlockSoundGroup.GRAVEL));
 
 
     public static void registerBlock() {
