330003ad2965a721b80fdf5b834ddc4692f14b6f
BuildTools <unconfigured@null.spigotmc.org>
2024-12-21 17:05:14 +0000

    Pizza Armour

--- src/main/java/blobbatobba/pizzamod/block/ModBlocks.java
+++ src/main/java/blobbatobba/pizzamod/block/ModBlocks.java
@@ -6,10 +6,12 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block TOMATO_CROP = Registry.register(Registries.BLOCK, new Identifier(PizzaMod.MOD_ID, "tomato_crop"),
